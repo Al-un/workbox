409e4e066601a18ce882569187d1cd88abf20b51
Al-un <alun.sng@gmail.com>
2019-01-03 16:23:10 +0100

    caching

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Getting started with Workbox https://developers.google.com/web/tools/workbox/gui
 1. [Getting started](https://developers.google.com/web/tools/workbox/guides/get-started)
    - Register service workers
    - Import workbox
+   - Caching content with a network first strategy
--- sw.js
+++ sw.js
@@ -4,8 +4,45 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js'
 );
 
+function settingUpWorkbox() {
+  // Cache JavaScript files
+  workbox.routing.registerRoute(
+    new RegExp('.*.js'),
+    workbox.strategies.networkFirst()
+  );
+
+  // Cache CSS files
+  workbox.routing.registerRoute(
+    /.*\.css/,
+    // Cached but updated in background ASAP
+    workbox.strategies.staleWhileRevalidate({
+      // custom cache name
+      cacheName: 'css-cache'
+    })
+  );
+
+  // Cache pictures
+  workbox.routing.registerRoute(
+    /.*\(?:png|jpg|jpeg|svg|gif)/,
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // custom name
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // max cache count
+          maxEntries: 20,
+          // cache for a week
+          maxAgeSeconds: 7 * 24 * 60 * 60
+        })
+      ]
+    })
+  );
+}
+
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
+  settingUpWorkbox();
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
